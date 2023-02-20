edbc8abb100f2c1198d30235e7dba79a8978df6d
Ben Burke <bernard.francis.burke@gmail.com>
2023-02-21 09:28:58 +1100

    at POC stage

--- pull_yt.py
+++ pull_yt.py
@@ -4,6 +4,7 @@ from pytube import YouTube
 import yt_dlp
 import ffmpeg
 import argparse
+import time
 
 arg_parser = argparse.ArgumentParser(description="Please provide a youtube or rumble url")
 
@@ -15,7 +16,15 @@ arguments = arg_parser.parse_args()
 
 def get_via_yt_dlp(url):
     print(url)
-    ytdl_opts = {'output': tempfile.gettempdir()}
+    #ytdl_opts = {'output': tempfile.gettempdir()}
+    ytdl_opts = {
+                'format': 'm4a/bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                }]
+    }
+
     with yt_dlp.YoutubeDL(ytdl_opts) as ytdl:
         ytdl.download([url])
         print(f'{ytdl.entries[0].title} downloaded to {tempfile.gettempdir()}')
@@ -27,8 +36,9 @@ def get_via_pytube(url, itag):
     yt.streams.filter(only_audio=True)
     temp_output = yt.streams.output_path = tempfile.gettempdir()
     stream = yt.streams.get_by_itag(itag)
-    stream.download(temp_output)
-    print(f'Downloaded {stream.title} to {temp_output}')
+    fp = tempfile.TemporaryFile()
+    stream.download(fp)
+    print(f'Downloaded {stream.title} to {fp}')
 
 
 if arguments.itag is not None:
@@ -40,15 +50,18 @@ print(f'itag: {itag}')
 
 fp = tempfile.TemporaryFile()
 
-
+tic = time.perf_counter()
 
 if "rumble" in arguments.url:
     print("rumble")
+    get_via_yt_dlp(arguments.url)
 else:
     print("getting from youtube")
     get_via_pytube(arguments.url, itag)
 
+toc = time.perf_counter()
 
+print(f'took {toc - tic:0.4f} seconds')
 
 # we'll make this smarter eventually with itags that might very from 22
 
