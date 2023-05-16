0a964b6c45022708ea0dcb822a2d897c52dcd263
Ben Burke <bernard.francis.burke@gmail.com>
2023-05-16 19:57:51 +1000

    I don't understand - but I must be close

--- sbs_searcher.sh
+++ sbs_searcher.sh
@@ -1,11 +1,12 @@
 TMP1=$(mktemp) 
 TMP2=$(mktemp)
 wget -q "https://www.sbs.com.au/api/video_feed/f/Bgtm9B/sbs-section-programs?form=json&q=$1&range=1-500" -O $TMP1 
-jq '.entries[].displayTitles.double.title + "@" + .entries[].displayTitles.double.subtitle + "@" + .entries[].id' $TMP1 | grep -i "$1"  | sort -u > $TMP2
-#cat $TMP2
-while IFS=@ read  show episode link;
+jq '.entries[].displayTitles.double.title + "," + .entries[].displayTitles.double.subtitle + "," + .entries[].id' $TMP1 | grep -i "$1"   > $TMP2
+sort -u $TMP2 > $TMP1
+#exit
+while IFS=, read  show episode link;
 do 
 	echo "# ${show} ${episode}"
 	echo http://www.sbs.com.au/ondemand/video/${link##*/}
-done<$TMP2
+done<$TMP1
 
