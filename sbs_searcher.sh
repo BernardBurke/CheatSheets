c492411e26e1562e3901247e8149f7389338b182
Ben Burke <bernard.francis.burke@gmail.com>
2023-05-16 19:16:57 +1000

    basic bits

--- sbs_searcher.sh
+++ sbs_searcher.sh
@@ -1,2 +1,6 @@
-wget -q "https://www.sbs.com.au/api/video_feed/f/Bgtm9B/sbs-section-programs?form=json&q='love hate'&range=1-100" -O sbs.json 
-jq '.entries[].displayTitles.double.title + "!" + .entries[].displayTitles.double.subtitle + "!" + .entries[].id' sbs.json | sort -u
+TMP1=$(mktemp) 
+TMP2=$(mktemp)
+wget -q "https://www.sbs.com.au/api/video_feed/f/Bgtm9B/sbs-section-programs?form=json&q=$1&range=1-500" -O $TMP1 
+jq '.entries[].displayTitles.double.title + "!" + .entries[].displayTitles.double.subtitle + "!" + .entries[].id' $TMP1 | grep -i "$1"  | sort -u > $TMP2
+cat $TMP2
+
