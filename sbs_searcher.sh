9243502f69efa9a5bf5816ea4a93394169ae29f8
Ben Burke <bernard.francis.burke@gmail.com>
2023-05-18 05:29:57 +1000

    sbs_searcher1

--- sbs_searcher.sh
+++ sbs_searcher.sh
@@ -1,7 +1,7 @@
 TMP1=$(mktemp) 
 TMP2=$(mktemp)
 wget -q "https://www.sbs.com.au/api/video_feed/f/Bgtm9B/sbs-section-programs?form=json&q=$1&range=1-500" -O $TMP1 
-jq '.entries[].displayTitles.double.title + "," + .entries[].displayTitles.double.subtitle + "," + .entries[].id' $TMP1 | grep -i "$1"   > $TMP2
+jq '.entries[].displayTitles.double.title + "," +  .entries[].displayTitles.double.subtitle + "," +   .entries[].id' $TMP1 | grep -i "$1"   > $TMP2
 sort -u $TMP2 > $TMP1
 #exit
 while IFS=, read  show episode link;
---
+++ sbs_searcher1.sh
@@ -0,0 +1,14 @@
+TMP1=$(mktemp) 
+TMP2=$(mktemp)
+wget -q "https://www.sbs.com.au/api/video_feed/f/Bgtm9B/sbs-section-programs?form=json&q=$1&range=1-500" -O $TMP1 
+#jq '.entries[] | .displayTitles.double.title, .displayTitles.double.subtitle, .id'
+jq  -r '.entries[] | .displayTitles.double.title, .displayTitles.double.subtitle, .id' $TMP1    > $TMP2
+#sort -u $TMP2 > $TMP1
+#cat $TMP2
+while IFS= read title; read episode; read link
+do 
+	echo yt-dlp -o \"${title} ${episode}.mp4\" http://www.sbs.com.au/ondemand/video/${link##*/}
+	#echo "${episode}"
+	#echo http://www.sbs.com.au/ondemand/video/${link##*/}
+done<$TMP2
+
