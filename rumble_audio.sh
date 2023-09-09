9eec2156c4e9a91427541bac3d9869f6f5ef9cf9
Ben Burke <bernard.francis.burke@gmail.com>
2023-09-10 07:22:24 +1000

    get smallest

--- rumble_audio.sh
+++ rumble_audio.sh
@@ -4,20 +4,23 @@ if [[ "$1" == "" ]]; then
 fi
 
 TMP1=$(mktemp) 
+TMP2=$(mktemp)
 
 yt-dlp --list-formats "$1" > $TMP1
 
-if grep -q mp4-360p-0 $TMP1; then
-	FORMAT=mp4-360p-0
-else
-	echo "formats available"
-	cat $TMP1
-	exit 1
-fi
+#if grep -q mp4-480p $TMP1; then
+#        FORMAT=mp4-480p
+#elif grep -q mp4-360p-0 $TMP1; then
+#	FORMAT=mp4-360p-0
+#else
+#	echo "formats available"
+#	cat $TMP1
+#	exit 1
+#fi
 
 read -p "Output Filename: " ANS
 
-yt-dlp -f $FORMAT -o $TMP "$1"
+yt-dlp -S '+size,+br' -o $ANS.mp4 "$1" 
 
-ffmpeg -i $TMP -vn -acodec copy "$ANS.aac"
+ffmpeg -i $ANS.mp4 -vn -acodec copy "$ANS.aac"
 	
