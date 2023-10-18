8cfe015d52f3cae14cf65cf6fc296119ae9bd029
Ben Burke <bernard.francis.burke@gmail.com>
2023-10-18 15:32:45 +1100

    subfolders and gif with image magic

--- rename_audio_to_subfolders.sh
+++ rename_audio_to_subfolders.sh
@@ -2,12 +2,14 @@ IFS=$'\n'
 for f in *; do
     #Skip directories
     [ -d "$f" ] && continue
-    tags=($(id3 -l "$f" | sed -re 's/\s{2,}([^:])/\n\1/g' | egrep ':.+'))
+    tags=($(id3v2 -l "$f" | sed -re 's/\s{2,}([^:])/\n\1/g' | egrep ':.+'))
     for l in ${tags[@]}; do
         [ -n "`echo $l | egrep '^Title'`" ] && TITLE="`echo $l | sed -re 's/^.*?: //'`"
         [ -n "`echo $l | egrep '^Artist'`" ] && ARTIST="`echo $l | sed -re 's/^.*?: //'`"
         [ -n "`echo $l | egrep '^Album'`" ] && ALBUM="`echo $l | sed -re 's/^.*?: //'`"
     done
-    mkdir -p "$ARTIST/$ALBUM"
-    mv "$f" "$ARTIST/$ALBUM/$TITLE.$(echo $f | sed -re 's/.*\.([^.]*$)/\1/g')"
+    #mkdir -p "$ARTIST/$ALBUM"
+    echo "$ARTIST/$ALBUM/$TITLE"
+    #echo "$ARTIST/$ALBUM/$TITLE.$(echo $f | sed -re 's/.*\.([^.]*$)/\1/g')"
+    #mv "$f" "$ARTIST/$ALBUM/$TITLE.$(echo $f | sed -re 's/.*\.([^.]*$)/\1/g')"
 done
