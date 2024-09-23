f678ad38860d59607a6df12b365cde91a8607173
Ben Burke <bernard.francis.burke@gmail.com>
2024-09-23 10:29:49 +1000

    arg

--- ffmpeg_helper.py
+++ ffmpeg_helper.py
@@ -10,5 +10,16 @@ arguments = arg_parser.parse_args()
 
 probe = ffmpeg.probe(arguments.filename)
 audio_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
+audio_stream = 
+output = audio_stream.output(acodec='copy','fred.aac')
+ffmpeg.run(output)
 
-print(audio_stream["codec_name"])
+# audio_stream = next((stream['codec_name'] for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
+# print(audio_stream)
+# input = ffmpeg.input(arguments.filename)
+# audio = input.audio
+# #audio = input.audio.acodec(audio_stream)
+# output = ffmpeg.output(audio,'fred.aac')
+
+
+#print(audio_stream["codec_name"])
